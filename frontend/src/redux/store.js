76677acd090c0708091a8c8f54f2df5d81503afa
Onkar Godase <onkarlgodase225@gmail.com>
2026-01-27 17:36:35 +0530

    Fix redux setup and dependency issues

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./userSlice";
+
+const persistConfig = {
+  key: "user",
+  storage,
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedUserReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
